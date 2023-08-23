637f6778fff8ffb081e7eb5d6381727ecf8989b7
Shree Chandan Sahu <sahuc1803@gmail.com>
2023-08-23 10:08:17 -0600

    commit_using_hashmap

--- 242.valid-anagram.java
+++ 242.valid-anagram.java
@@ -4,19 +4,28 @@
  * [242] Valid Anagram
  */
 
-// @lc code=start
+// @lc lets Solve using hash table 
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char [] sArr = s.toCharArray();
-        char [] tArr = t.toCharArray();
+       HashMap <Character, Integer> s1= new HashMap<>();
+      HashMap <Character, Integer> t1= new HashMap<>();
 
-        Arrays.sort(sArr);
-        Arrays.sort(tArr);
+      if (s.length()!=t.length()) return false;
 
-        return Arrays.equals(sArr,tArr);
+      for(int i=0; i<s.length(); i++){
+          char ch= s.charAt(i);
+          s1.put(ch,s1.getOrDefault(ch, 0)+1);
+      }
+
+      for(int j=0; j<t.length(); j++){
+          char ch2=t.charAt(j);
+          t1.put(ch2,t1.getOrDefault(ch2,0) +1);
+      }
+            return s1.equals(t1);
+     
      }
 }
 // @lc code=end
